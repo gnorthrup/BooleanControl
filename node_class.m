e9620ff6c55b3af68afde7cf198d99032f8288f7
Adam Gruenbaum <agtree@cs.uchicago.edu>
2017-02-13 00:55:01 -0600

    added constructor to node_class

--- node_class.m
+++ node_class.m
@@ -6,6 +6,11 @@ classdef node_class
         %signal_delay
     end
     methods
+        function node = node_class(gt, c1, c2)
+            node.gatetype = gt;
+            node.child1 = c1;
+            node.child2 = c2;
+        end
         function f = eval(self, v)
             if(self.gatetype == gatetype.AND)
                 f = and(eval(self.child2,v), eval(self.child1,v));
