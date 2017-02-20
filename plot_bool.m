6fcf5f60ab857b8792868d1ae1b2ee6481d22a55
Adam Gruenbaum <agtree@cs.uchicago.edu>
2017-02-20 15:11:46 -0600

    added a plotting function

---
+++ plot_bool.m
@@ -0,0 +1,21 @@
+function [  ] = plot_bool( nodestates, start, stop, name, nodenames )
+% produces a clean, readible, plot of a boolean model from start to end
+    %apply an offset so that each node can be read individually
+    offset = .05;
+    nodestates = nodestates(:,start:stop);
+    for i = 1:size(nodestates, 1) / 2
+        nodestates(2*i - 1,:) = nodestates(2*i - 1,:) + offset*i;
+        nodestates(2*i,:) = nodestates(2*i,:) - offset*i;
+    end
+    %plot nodes
+    figure;
+    hold all;
+    for i = 1:size(nodestates,1)
+        plot(nodestates(i,:));
+    end
+    title(name);
+    legend(nodenames);
+    yticks([0 1])
+    hold off;
+end
+
