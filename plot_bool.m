fc71c6a7fef0b3d1005fecf43703a9071febe303
Adam Gruenbaum <agtree@cs.uchicago.edu>
2017-02-22 14:16:28 -0600

    pretty light plots

--- plot_bool.m
+++ plot_bool.m
@@ -3,17 +3,28 @@ function [  ] = plot_bool( nodestates, start, stop, name, nodenames, lightnodes)
     %apply an offset so that each node can be read individually
     offset = .05;
     nodestates = nodestates(:,start:stop);
+    lightstates = nodestates(lightnodes(1),:);
     nodestates(lightnodes,:) = [];
     for i = 1:size(nodestates, 1) / 2
         nodestates(2*i - 1,:) = nodestates(2*i - 1,:) + offset*i;
         nodestates(2*i,:) = nodestates(2*i,:) - offset*i;
     end
+    
+    lighton = ones(2,size(nodestates,2))* 0.5;
+    lighton(1,:) = 1:size(nodestates,2);
+    
+    lightoff = lighton(:,lightstates == 0);
+    lighton = lighton(:, lightstates == 1);
+        
     %plot nodes
+    
     figure;
     hold all;
     for i = 1:size(nodestates,1)
         plot(nodestates(i,:));
     end
+    scatter(lighton(1,:),lighton(2,:),[],'y','filled')
+    scatter(lightoff(1,:),lightoff(2,:),[],'k','filled')
     title(name);
     legend(nodenames);
     yticks([0 1])
