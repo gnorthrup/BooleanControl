b0cd5e0b0f557e8cb32fd2e5ec2499fe1ba1027f
gnorthrup <gnorthrup@uchicago.edu>
2017-02-27 16:07:42 -0600

    The paper model has appropriate behavior?

--- papermodel.m
+++ papermodel.m
@@ -4,7 +4,7 @@ clear all
 close all
 
 NUMNODES = 7;
-NUMSTEPS = 200;
+NUMSTEPS = 1000;
 t_dawn = 12;
 t_dusk = 36;
 
@@ -19,9 +19,11 @@ nodestates = zeros(NUMNODES, NUMSTEPS + 1);
 %L2
 %L3
 
-t = [1.5, 5.5, 6.5, 0, 7.5, 4, 0, 2.5, 1];
+%t = [1.5, 5.5, 6.5, 0, 7.5, 4, 0, 2.5, 1];
+%t = [3,6,7.5,0.5,6,4,1,0.5,0];
 
-t = t*2;
+t = [5,11,14,0,11,7,1,0,0];
+%t = t*2;
 
 LHYeval = eval_tree(gatetype.AND, fetch_node(t(3),3), fetch_node(t(7),5));
 TOC1eval = eval_tree(gatetype.AND, eval_tree(gatetype.NOT, fetch_node(t(1),1)), fetch_node(t(6),4));
@@ -32,15 +34,6 @@ L1eval = eval_tree(gatetype.ID, light_node(t_dawn, t_dusk));
 L2eval = eval_tree(gatetype.ID, light_node(t_dawn, t_dusk));
 L3eval = eval_tree(gatetype.ID, light_node(t_dawn, t_dusk));
 
-LHYeval = eval_tree(gatetype.AND, fetch_node(t(3),3), fetch_node(t(7), 5));
-TOC1eval = eval_tree(gatetype.AND, eval_tree(gatetype.NOT, fetch_node(t(1),1)), fetch_node(t(6), 4));
-Xeval = eval_tree(gatetype.ID,fetch_node(t(2),2));
-Yeval = eval_tree(gatetype.AND, eval_tree(gatetype.AND, eval_tree(gatetype.NOT, fetch_node(t(4), 1)) ...
-    ,eval_tree(gatetype.NOT, fetch_node(t(5), 2))), eval_tree(gatetype.OR, fetch_node(t(8), 6), fetch_node(t(9),7)));
-L1eval = eval_tree(gatetype.ID, light_node(t_dawn, t_dusk));
-L2eval = eval_tree(gatetype.ID, light_node(t_dawn, t_dusk));
-L3eval = eval_tree(gatetype.ID, light_node(t_dawn, t_dusk))
-
 
 eval_array = [LHYeval, TOC1eval, Xeval, Yeval, L1eval, L2eval, L3eval];
 
@@ -50,7 +43,7 @@ for i=2:NUMSTEPS
     end
 end
 
-plot_bool(nodestates, 1, 200, 'trying to do a thing', ...
-    {'LHYeval', 'TOC1eval', 'Xeval', 'Yeval', 'L1eval', 'L2eval', 'L3eval'}, [5,6,7])
+plot_bool(nodestates, 800, 1000, 'trying to do a thing', ...
+    {'LHYeval', 'TOC1eval', 'Xeval', 'Yeval', 'Day','Night'}, [5,6,7])
 
 disp('done')
